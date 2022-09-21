a8c0ab0675234b668c9f1e574321cd537bf43e6f
AHenry2117 <105332403+AHenry2117@users.noreply.github.com>
2022-09-20 21:13:36 -0700

    Update commands.js

--- commands.js
+++ commands.js
@@ -90,9 +90,16 @@ db.bounties.find({reward: {$gte:1000}})
 db.bounties.find({species:"Groundhog"},{location:"Woodlands"})
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
-
+db.bounties.updateOne(
+  {name: "Polarwind"},
+  {$set: {reward: 1000}}
+  )
 // 2. Remove Lokinkajou
-
+db.bounties.deleteOne({name:"Lokinkajou"})
 // 3. Delete all bounties sent by Songbird
-
+db.bounties.deleteMany({client:"Songbird"})
 // 4. Update all captured statuses to true
+db.bounties.updateMany(
+  {captured: false},
+  {$set: {captured: true}}
+  )
